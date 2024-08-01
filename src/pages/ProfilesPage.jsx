3d9d79d94b5fa6ceb35ba7b702f129664f0b9c56
Anup-Banstola <anupin09@gmail.com>
2024-08-01 16:07:10 +0545

    testing

--- src/pages/ProfilesPage.jsx
+++ src/pages/ProfilesPage.jsx
@@ -1,10 +1,10 @@
-// import ProfilesList from "../components/Profiles";
+import ProfilesList from "../components/Profiles";
 
 const ProfilesPageList = () => {
   return (
     <div>
       <h1 className="text-center font-bold text-xl my-8">Profiles</h1>
-      {/* <ProfilesList /> */}
+      <ProfilesList />
     </div>
   );
 };
