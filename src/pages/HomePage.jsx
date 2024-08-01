b11ef8261ec46348311135fcfee2d9ffca6946b0
Anup-Banstola <anupin09@gmail.com>
2024-08-01 14:32:18 +0545

    Added heading to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import FormComponent from "../components/FormComponent";
 const HomePage = () => {
   return (
     <div>
+      <h1 className="text-center mt-8 text-xl font-semibold">React Coding </h1>
       <FormComponent />
     </div>
   );
