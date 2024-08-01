b759fd21d38a0c661b8dab2164920a4f2e2469f0
Anup-Banstola <anupin09@gmail.com>
2024-08-01 14:23:14 +0545

    solved page not found issue of profiles

--- src/pages/ProfilesPage.jsx
+++ src/pages/ProfilesPage.jsx
@@ -1,10 +1,10 @@
-import Profiles from "../components/Profiles";
+import ProfilesList from "../components/Profiles";
 
 const ProfilesPage = () => {
   return (
     <div>
       <h1 className="text-center font-bold text-xl my-8">Profiles</h1>
-      <Profiles />
+      <ProfilesList />
     </div>
   );
 };
