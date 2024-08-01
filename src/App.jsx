0f69c9cd2a2ddc147da4f748c9e3cd914ab4530a
Anup-Banstola <anupin09@gmail.com>
2024-08-01 15:57:46 +0545

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import ProfilesPage from "./pages/ProfilesPage";
+
+import ProfilesPageList from "./pages/ProfilesPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<HomePage />} />
-        <Route path="/profiles" element={<ProfilesPage />} />
+        <Route path="/profiles" element={<ProfilesPageList />} />
       </Routes>
     </BrowserRouter>
   );
